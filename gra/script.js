73b0a20f725fd50207301f430b8e8ed607e2e99b
Wiktoria Wlodarczyk <wlodarczykw033@gmail.com>
2023-03-01 13:39:06 +0100

    poprawki

--- gra/script.js
+++ gra/script.js
@@ -4,34 +4,68 @@ window.addEventListener('DOMContentLoaded', (event) => {
     var gap = document.getElementById("gap");
     var block = document.getElementById("block");
     var bird = document.getElementById("bird");
+    var counter =0;
+    var flying =0;
+
     
     
+   
 
     gap.addEventListener("animationiteration", () => {
         var random = -((Math.random() * 375) + 125);
-        //console.log(random);
         gap.style.top = random + "px";
+        counter ++;
 
-    });
-   
+    })
+
     
-    document.addEventListener("keyup", event => {
-        if (event.code ==='Space'){
-            fly();
+   
+    setInterval(function () {
+        var birdTop=parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
+        if (flying === 0)
+        {
+            bird.style.top = (birdTop +2) + "px";
+        }
+        var blockSide=parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+        var gapTop=parseInt(window.getComputedStyle(gap).getPropertyValue("top"));
+        var cTop= (gapTop - 200);
+        var dTop= (birdTop - 30);
+       
+        if ((birdTop  > -250)||((blockSide < 150) && (blockSide > 80) && ((dTop > gapTop) || (birsTop < cTop)))) {
             
+            alert("END GAME. SCORE: "+ (counter-1));
+            bird.style.top = -400 + "px";
+            counter = 0;
+        }
+    }, 10);
+    
+    
+    function fly() {
+        console.log(bird.style.top);
+        flying = 1; 
+        let flyCount = 0;
+        
+        var flyInterval = setInterval(() => {
+            var birdTop = parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
+             
+            if((birdTop > -700) && (flyCount < 20)) {
+                bird.style.top = (birdTop - 5) + "px"
+             }
+            if(flyCount > 20) {
+                clearInterval(flyInterval);
+                flying = 0;
+                flyCount = 0; nn   
+            }
+            flyCount++
+        }, 20);
+
+    }
+    
+    document.addEventListener('keyup', event => {
+        if (event.code === 'Space') {
+            fly()
         }
     })
-    setInterval(gravity, 10);
-
-
-    function fly(){
-        var jump =  parseInt(window.getComputedStyle(bird).getPropertyValue("top")) - 70;
-           bird.style.top = jump + "px";
-    }
-    function gravity(){
-        var down = parseInt(window.getComputedStyle(bird).getPropertyValue("top")) +2;
-        bird.style.top = down + "px";
-    }
 });
 
  
